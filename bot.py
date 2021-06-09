6544192457be262fce7bac11cfdb7a6c5e5d0b45
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-09 21:59:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ class Bot(Client):
             api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.TG_BOT_TOKEN,
-            plugins={"root": "mwk/rename"},
+            plugins={"root": "deeks/rename"},
             sleep_threshold=5,
         )
 
