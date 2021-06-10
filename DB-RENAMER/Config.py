396c54fbc5ffa06d3a4972b838b9b89faca7ea8a
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-10 07:23:00 +0530

    Create Config.py

--- DB-RENAMER/Config.py
+++ DB-RENAMER/Config.py
@@ -2,20 +2,20 @@ import os
 
 class Config(object):
 
-  APP_ID = int(os.environ.get("APP_ID", ""))
+  APP_ID = int(os.environ.get("APP_ID", "2980496"))
 
-  API_HASH = os.environ.get("API_HASH", "")
+  API_HASH = os.environ.get("API_HASH", "9415a61fedcc0f00f33667ca46e577a3")
 
-  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1705260996:AAHfaDQeXP2ft11YF3OR4CMJccTeRxIK5VE")
 
-  AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+  AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1086432320").split(1051512795))
 
   DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
 
   DB_URI = os.environ.get("DATABASE_URL", "")
 
-  OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
+  OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1086432320").split("1051512795")]
 
   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",False)
 
-  UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+  UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "DB_ROBOTS")
