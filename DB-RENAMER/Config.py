053d04d76123b23316bab1efb14b9e4804fb6c47
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-06-10 10:43:34 +0530

    Update Config.py

--- DB-RENAMER/Config.py
+++ DB-RENAMER/Config.py
@@ -8,11 +8,11 @@ class Config(object):
 
   TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1705260996:AAHfaDQeXP2ft11YF3OR4CMJccTeRxIK5VE")
 
-  AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1086432320").split(1051512795))
+  AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "Deeks_04_8").split())
 
   DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
 
-  DB_URI = os.environ.get("DATABASE_URL", "")
+  DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Prorenamer: Prorenamer@renamerpro.2sj6d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
   OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1086432320").split("1051512795")]
 
